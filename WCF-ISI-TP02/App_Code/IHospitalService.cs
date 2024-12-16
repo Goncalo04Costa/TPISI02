773e942f88359d745f3af08385a01d7e16893ad6
Goncalo04Costa <a26024@alunos.ipca.pt>
2024-12-16 16:59:54 +0000

    ISI WCF
    ISI WCF

--- WCF-ISI-TP02/App_Code/IHospitalService.cs
+++ WCF-ISI-TP02/App_Code/IHospitalService.cs
@@ -6,22 +6,6 @@ using System.ServiceModel;
 [ServiceContract]
 public interface IHospitalService
 {
-    // Funcionario Methods
-    [OperationContract]
-    List<Funcionario> GetAllFuncionarios();
-    [OperationContract]
-    Funcionario GetFuncionarioById(int id);
-
-    // Utente Methods
-    [OperationContract]
-    List<Utente> GetAllUtentes();
-    [OperationContract]
-    List<Utente> GetUtentesById();
-
-    // Medico Methods
-    [OperationContract]
-    List<Medico> GetAllMedicos();
-    [OperationContract]
-    List<Medico> GetMedicoById();
+  
 
 }
